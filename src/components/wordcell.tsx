80815294b7449ca1b64892597b3b22b2a1b0eb00
Ramon Medeiros <ramon.rnm@gmail.com>
2026-05-17 08:42:14 +0200

    remove error

--- src/components/wordcell.tsx
+++ src/components/wordcell.tsx
@@ -1,10 +1,14 @@
-import CrossWordGame from "../services/crossword/crossword";
 import "./wordcell.css"
 import { Input } from '@mui/material';
 
+interface WordcellProps {
+    wordIndex: number;
+    letterIndex: number;
+    value: string;
+}
 
-const Wordcell = (wordIndex: number, letterIndex: number, game: CrossWordGame) => {
-    return <div key={wordIndex} className="wordcel">
+const Wordcell = ({ wordIndex, letterIndex, value }: WordcellProps) => {
+    return <div className="wordcel">
         <Input
             key={`${wordIndex}-${letterIndex}`}
             type="text"
@@ -13,10 +17,8 @@ const Wordcell = (wordIndex: number, letterIndex: number, game: CrossWordGame) =
             }}
             maxRows={1}
             multiline={false}
-            value={game.getUserInputByCoordenates(wordIndex, letterIndex)}
-            //onChange={(e) => handleInputChange(index, colIndex, e.target.value)}
+            value={value}
             className="w-10 h-10 text-center m-1"
-            //disabled={char !== ''}
         />
     </div>
 }
